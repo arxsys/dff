fd38262edd27d747ad728b6c323513b539e6678a
fba <fba@arxsys.fr>
2013-02-27 13:13:47 +0100

    Updating years in headers

--- testsuite/mfso/mfso_contiguous.py
+++ testsuite/mfso/mfso_contiguous.py
@@ -1,5 +1,5 @@
 # DFF -- An Open Source Digital Forensics Framework
-# Copyright (C) 2009-2011 ArxSys
+# Copyright (C) 2009-2013 ArxSys
 # This program is free software, distributed under the terms of
 # the GNU General Public License Version 2. See the LICENSE file
 # at the top of the source tree.
